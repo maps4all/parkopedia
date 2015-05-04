008040ae25c4add0e911f2468a62291d1760a17a
maps4all <maps4all007@yandex.com>
2015-05-04 11:42:59 +0200

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -17,12 +17,29 @@
     "browserLaunchLink" : "http://www.parkopedia.mobi/#/map/#lat#,#lng#",
     
     "onPageFinishedLoadUrl" : "javascript:
+
 $.getScript('http://cdn.jsdelivr.net/jquery.cookie/1.4.1/jquery.cookie.min.js', function(){
-    var str = $.cookie('EXCLUDE_FROM_EXPERIMENT');
+    
+var str = $.cookie('EXCLUDE_FROM_EXPERIMENT');
     strOut = '1';        
-    $.cookie('EXCLUDE_FROM_EXPERIMENT', strOut);          
-    $('#fullscreen-overlay').hide();
+    $.cookie('EXCLUDE_FROM_EXPERIMENT', strOut);
+    var d = new Date();      
+    $('#fullscreen-overlay').hide();    
+    m = app.map.getMap();     
+    waitAndRetry(0, 4);     
+});
+
+function waitAndRetry(count, max) { 
+     
+    var millisecondsToWait = 250;
+    if (count >= max) return;
     m = app.map.getMap();
-    m.setZoom(#zoom#);      
-});"
+    if (m) {    
+        m.setZoom(#zoom#);      
+    } else {       
+        setTimeout(function() {
+            waitAndRetry(count++, max);
+        }, millisecondsToWait);
+    }    
+}"
 }
